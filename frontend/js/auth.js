9e2cfb95fbd23ca3d2904018100f0be84181230e
emthy0 <ironpan10@hotmail.com>
2023-05-03 08:00:35 +0700

    fix api address

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,5 +1,5 @@
 // TODO #4.0: Change this IP address to EC2 instance public IP address when you are going to deploy this web application
-const backendIPAddress = "127.0.0.1:3000";
+const backendIPAddress = "3.217.142.246/api/v1";
 
 const authorizeApplication = () => {
   window.location.href = `http://${backendIPAddress}/auth/auth_app`;
